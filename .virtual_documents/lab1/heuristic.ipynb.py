c19a060b0bd9a1f5b9b0b13c37237ac538a29ebc
Pushkar Patel <42088801+thepushkarp@users.noreply.github.com>
2021-01-23 02:26:07 +0530

    Add heuristic functions

---
+++ .virtual_documents/lab1/heuristic.ipynb.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+
+def mat_to_dict(matrix):
+    """
+    Converts goal state to dictionary to get indices easier
+    
+    Parameters:
+    matrix(np.ndarray): A 3x3 numpy array with each cell containing unique elements
+    
+    Returns:
+    mat_dict(dict[int, tuple[int, int]]): A mapping of cell contents to a tuple
+    contianing its indices
+    """
+    mat_dict = {}
+    for i in range(len(matrix)):
+        row = matrix[i];
+        for j in range(len(row)):
+            value = row[j];
+            mat_dict[value] = (i, j)
+    return mat_dict
+
+
+def h1(curr_state, goal_dict):
+    """
+    Heuristic for calculating the distance of goal state using Manhattan distance
+    
+    Parameters:
+    curr_state(np.ndarray): A 3x3 numpy array with each cell containing unique elements
+    goal_dict(dict[int, tuple[int, int]]): A mapping of cell contents to a tuple
+    contianing its indices
+    
+    Returns:
+    h(int): Heuristic value
+    """
+    h = 0
+    for i in range(curr_state.shape[0]):
+        for j in range(curr_state.shape[1]):
+            value = curr_state[i][j]
+            x = goal_dict[value][0]
+            y = goal_dict[value][1]
+            h += abs(i-x) + abs(j-y)
+    return h
+
+
+def h2(curr_state, goal_dict):
+    """
+    Heuristic for calculating the distance of goal state using number of misplaced tiles
+    
+    Parameters:
+    curr_state(np.ndarray): A 3x3 numpy array with each cell containing unique elements
+    goal_dict(dict[int, tuple[int, int]]): A mapping of cell contents to a tuple
+    contianing its indices
+    
+    Returns:
+    h(int): Heuristic value
+    """
+    h = 0
+    for i in range(curr_state.shape[0]):
+        for j in range(curr_state.shape[1]):
+            value = curr_state[i][j]
+            x = goal_dict[value][0]
+            y = goal_dict[value][1]
+            if ((x, y) get_ipython().getoutput("= (i, j)):")
+                h += 1
+    return h
+
+
+GOAL_STATE = np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
+print("Goal State:\n", GOAL_STATE)
+
+
+goal_dict = mat_to_dict(GOAL_STATE)
+print("Goal dict:", goal_dict)
+
+
+# Change this to test
+CURR_STATE = np.array([[0, 2, 3], [1, 8, 4], [7, 6, 5]])
+print("Current State:\n", CURR_STATE)
+
+
+# Heuristic 1
+print("Value from h1:", h1(CURR_STATE, goal_dict))
+
+
+# Heuristic 2
+print("Value from h2:", h2(CURR_STATE, goal_dict))
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # cs362-naagraaj
+
 Codes for CS326 Assignments
+
+## Team Members
+
+-   Aditya Prakash (201851008)
+-   Anuj Puri (201851025)
+-   Ashutosh Singh (201851029)
+-   Pushkar Patel (201851094)
+-   Yash Shah (201851150)
+
+## Some General Notes
+
+-   Preferably use [Jupyter Lab](http://jupyterlab.io/) to open the notebooks locally.
+-   To open it as an excutable in [Google Colab](https://colab.research.google.com/), refer [here](https://colab.research.google.com/github/googlecolab/colabtools/blob/master/notebooks/colab-github-demo.ipynb#scrollTo=Rmai0dD30XzL).
+-   To view the ipynb notebooks online in read-only format, use [nbviewer](https://nbviewer.jupyter.org/).
